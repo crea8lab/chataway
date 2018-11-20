96f7ad1f9511d0d7958e89e6b91f540a99d33c59
Fayvor George <phavorsparks@gmail.com>
2018-11-20 19:29:06 +0100

    add moment to server and client

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,16 +1,18 @@
+const moment = require('moment')
+
 module.exports = {
   generateMessage (from, text) {
     return {
       from,
       text,
-      createdAt: new Date().getTime()
+      createdAt: moment().valueOf()
     }
   },
   generateLocationMessage (from, lat, lng) {
     return {
       from,
       url: `https://www.google.com/maps?q=${lat},${lng}`,
-      createdAt: new Date().getTime()
+      createdAt: moment().valueOf()
     }
   }
 }

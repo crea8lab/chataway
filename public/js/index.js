5301084ebc607e152d816a03d8ac64423c25f441
Fayvor George <phavorsparks@gmail.com>
2018-11-20 11:59:01 +0100

    set up minimal UI with jQuery to show up messages

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,23 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('New Message', message)
+
+  let li = $('<li></li>')
+  li.text(`${message.from}: ${message.text}`)
+
+  $('#messages').append(li)
+})
+
+$('#message-form').on('submit', function (e) {
+  e.preventDefault()
+  
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function () {
+    
+  })
+
+  // clear input field
+  $('[name=message]').val('')
 })
--- server/server.js
+++ server/server.js
@@ -24,16 +24,12 @@ io.on('connection', function (socket) {
   // Broadcast new user joined
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message)
 
     io.emit('newMessage', generateMessage(message.from, message.text))
 
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // })
+    callback('recieved')
   })
 
   // on disconnect

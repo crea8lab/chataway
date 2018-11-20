e9e0b95c0466e6c0bd038307e334e87e19823d96
Fayvor George <phavorsparks@gmail.com>
2018-11-20 10:43:27 +0100

    emit new message to all  connected users

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io()
 
 socket.on('connect', function () {
   console.log('Connected to server')
-
-  socket.emit('createMessage', {
-    from: 'scala',
-    text: 'Hey, I am having fun'
-  })
 })
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -17,14 +17,14 @@ app.use(express.static(publicPath))
 io.on('connection', function (socket) {
   console.log('New user connected')
 
-  socket.emit('newMessage', {
-    from: 'Valda',
-    text: 'How you doing',
-    createdAt: 33132123
-  })
-
   socket.on('createMessage', (message) => {
-    console.log('Message', message)
+    console.log('createMessage', message)
+
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   })
 
   // on disconnect

6dc717023285a1679b49ca604c645e93a65f4a27
Fayvor George <phavorsparks@gmail.com>
2018-11-20 10:57:10 +0100

    greet new user and broadcast to others

--- server/server.js
+++ server/server.js
@@ -17,6 +17,19 @@ app.use(express.static(publicPath))
 io.on('connection', function (socket) {
   console.log('New user connected')
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to chattaway',
+    createdAt: new Date().getTime()
+  })
+
+  // Broadcast new user joined
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message)
 
@@ -25,6 +38,12 @@ io.on('connection', function (socket) {
       text: message.text,
       createdAt: new Date().getTime()
     })
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // })
   })
 
   // on disconnect

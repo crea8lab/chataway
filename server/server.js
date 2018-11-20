ac839bbd42c3aee89a5452e9d511fa1c53258603
Fayvor George <phavorsparks@gmail.com>
2018-11-20 10:09:24 +0100

    add connect and disconnect functionalities

--- server/server.js
+++ server/server.js
@@ -2,14 +2,26 @@ require('../config/config')
 
 const path = require('path')
 const express = require('express')
+const socketIO = require('socket.io')
+const http = require('http')
 
 const app = express()
+const server = http.createServer(app)
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT
 const uri = process.env.MONGODB_URI
+const io = socketIO(server)
 
 app.use(express.static(publicPath))
 
-app.listen(port, () => console.log(`app running on port ${port}`))
+io.on('connection', (socket) => {
+  console.log('New user connected')
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected')
+  })
+})
+
+server.listen(port, () => console.log(`app running on port ${port}`))
 
 module.exports = app

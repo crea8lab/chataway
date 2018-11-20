5ba25f319f219632ec6e9d8b0969fc402195b430
Fayvor George <phavorsparks@gmail.com>
2018-11-20 21:37:47 +0100

    setup users class

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,12 +18,25 @@ function scrollToBottom() {
 }
 
 socket.on('connect', function () {
-  console.log('Connected to server')
+  let params = $.deparam(window.location.search)
+
+  socket.emit('join', params, function (err) {
+    if (err) {
+      alert(err)
+      window.location.href = '/'
+    } else {
+      console.log('no error')
+    }
+  }) 
 })
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server')
 }) 
+
+socket.on('updateUserList', function (users) {
+  console.log('users list', users)
+})
 
 socket.on('newMessage', function (message) {
   let formattedTime = moment(message.createdAt).format('h:mm a')
--- server/server.js
+++ server/server.js
@@ -13,16 +13,32 @@ const uri = process.env.MONGODB_URI
 const io = socketIO(server)
 
 const { generateMessage, generateLocationMessage } = require('./utils/message')
+const { isRealString } = require('./utils/validation')
+const Users = require('./utils/users')
+
+const users = new Users()
 
 app.use(express.static(publicPath))
 
 io.on('connection', function (socket) {
   console.log('New user connected')
 
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to chattaway'))
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      return callback('Name and room name are required')
+    }
 
-  // Broadcast new user joined
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
+    socket.join(params.room)
+    users.removeUser(socket.id)
+    users.addUser(socket.id, params.name, params.room)
+
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room))
+
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to chattaway'))
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`))
+
+    callback()
+  })
 
   socket.on('createMessage', (message, callback) => {
     io.emit('newMessage', generateMessage(message.from, message.text))
@@ -36,7 +52,13 @@ io.on('connection', function (socket) {
 
   // on disconnect
   socket.on('disconnect', function () {
-    console.log('Client disconnected')
+    let user = users.removeUser(socket.id)
+
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room))
+
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left`))
+    }
   })
 })
 
